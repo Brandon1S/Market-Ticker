ac0733807999f37d577ca04f20e09ab140ba207e
eggnaug <nicolas.naugle@gmail.com>
2023-12-10 23:03:47 -0500

    Fixed main call

--- main.py
+++ main.py
@@ -13,3 +13,11 @@ client = RESTClient(api_key=os.environ['POLYGON_API_CLIENT'])
 # 2. read through list of tickers in a for loop
 #       get all end-of-day data over the course of the last year with daily candles
 #
+
+
+def main():
+    print("'ello gov'na")
+
+
+if __name__=="__main__":
+    main()
