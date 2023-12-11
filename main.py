ed32afb04e977f27e24ffd0a0bc51ff6a1c6b7f5
eggnaug <nicolas.naugle@gmail.com>
2023-12-10 22:52:30 -0500

    Added initial main file with polygon API connection

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+from polygon import RESTClient
+
+client = RESTClient(api_key=os.environ['POLYGON_API_CLIENT'])
+
+# TODO 
+# 1. get a list of tickers. 
+#       auto generated based on criteria
+#       put that shit into a separate text file
+#       OR scan all tickers every time to catch any new ones that come up
+#       or ones that fall off
+
+# 2. read through list of tickers in a for loop
+#       get all end-of-day data over the course of the last year with daily candles
+#
